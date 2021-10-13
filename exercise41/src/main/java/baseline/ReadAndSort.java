a6521b1969aa6c881b4bd356f3f2da5eb951dcff
diego <diegofigueroa0519@gmail.com>
2021-10-13 18:08:13 -0400

    exercise 41 speudo

---
+++ exercise41/src/main/java/baseline/ReadAndSort.java
@@ -0,0 +1,15 @@
+package baseline;
+
+public class ReadAndSort {
+    //Create reads method the input text file
+        //Method places all names in an arrayList
+        //catch (NoSuchElementException | IllegalStateException | IOException e | FileNotFoundException)
+
+    //Create sort method that sorts all names
+        //arrayList has a command method called <.sort> in which it sorts all in list
+        //catch (NoSuchElementException | IllegalStateException | IOException e | FileNotFoundException)
+
+    //Create write method which prints number of names, followed by "-----" and then the names in order
+    ////catch (NoSuchElementException | IllegalStateException | IOException e | FileNotFoundException)
+
+}
---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,14 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Diego Figueroa
+ */
+
+public class Solution41 {
+    //try to read file
+    //catch (NoSuchElementException | IllegalStateException | IOException e | FileNotFoundException)
+
+    //try to sort and write file
+    //catch (NoSuchElementException | IllegalStateException | IOException e | FileNotFoundException)
+}
