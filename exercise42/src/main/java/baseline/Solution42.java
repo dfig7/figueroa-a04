f168a7a3f8f0302603f5edd5441a041a1268ea2c
diego <diegofigueroa0519@gmail.com>
2021-10-14 12:54:55 -0400

    exercise 42 pseudo Updated

--- exercise42/src/main/java/baseline/Solution42.java
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -6,10 +6,24 @@ package baseline;
  */
 
 public class Solution42 {
-    //call ReadAndWrite
+
+    //method that reads the file and stores data into Employee class
+    //use a while loop to store all names in an ArrayList
+
+    //have a method that prints every employee
+    //String(counter: data)
+        //use method in employee class that prints employees parsed
+
+
+
+
+}
+
+    /*
+    //call Employee
     //try to read file
     //catch FileNotFoundException
 
     //try to print to output file
     //catch FileNotFoundException
-}
+*/
