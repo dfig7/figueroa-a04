02516b0458f8672a3d4838441d45b4e02b2f0fcf
diego <diegofigueroa0519@gmail.com>
2021-10-14 12:22:15 -0400

    exercise 42 pseudo

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,15 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Diego Figueroa
+ */
+
+public class Solution42 {
+    //call ReadAndWrite
+    //try to read file
+    //catch FileNotFoundException
+
+    //try to print to output file
+    //catch FileNotFoundException
+}
