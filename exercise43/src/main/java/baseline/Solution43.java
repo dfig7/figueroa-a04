35fadecf0774b6f47bbd53f59b674ddc4b79ab34
diego <diegofigueroa0519@gmail.com>
2021-10-15 14:13:07 -0400

    exercise 43 pseudo

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,29 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Diego Figueroa
+ */
+
+public class Solution43 {
+    //do code all in one main
+
+
+    //ask user for name of site
+        //store in siteName
+
+    //ask user for name of author
+        //store in siteAuthor
+
+    //ask user if want folder for javascript
+        //answer is in char --> can either be y || Y || yes
+
+    //ask user if want folder for CSS
+        //answer is in char --> can either be y || Y || yes
+
+    //create website
+        //generate an index.html file
+            //place siteName inside <title> tag
+            //place siteAuthor inside <meta> tag
+
+}
