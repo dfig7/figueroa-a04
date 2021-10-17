53f87d248f6dcfcd926b58d5a2cb620a5186df93
diego <diegofigueroa0519@gmail.com>
2021-10-17 18:56:33 -0400

    exercise 45 PSeudo

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,15 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Diego Figueroa
+ */
+
+
+public class Solution45 {
+    //main
+        //creates WordFinder
+        //replaces instances
+        //get Text
+
+}
---
+++ exercise45/src/main/java/baseline/WordFinder.java
@@ -0,0 +1,17 @@
+package baseline;
+
+public class WordFinder {
+    //String fileText
+    public String fileText = "";
+
+    //method getText
+        //return fileText
+
+    //method readFile
+        //reads file by looping through each line and storing into fileText
+
+    //method replaceText
+        //method replaces instances of utilize to use
+        //replaceAll command
+
+}
