12c0652c42c283b41d4d51562bc6a710cb1a8850
diego <diegofigueroa0519@gmail.com>
2021-10-17 12:27:14 -0400

    exercise 44 pseudo

---
+++ exercise44/src/main/java/baseline/ProductSearch.java
@@ -0,0 +1,33 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Diego Figueroa
+ */
+
+public class ProductSearch {
+    //Array productList
+    //Product product
+    //boolean found
+
+    //method Array readData
+        //reads the JSON file and puts each product in an array;
+        //calls Product class
+            //stores product name, price, and quantity into class
+        //return list
+
+    //method Boolean findProduct(
+        //finds product in productList
+            //returns true;
+
+
+    //method String getProduct
+        //asks user for product
+            //product = nextline
+        //while(found)
+            //find product in product list
+                //print out
+        //if not found
+            //print sorry not found and ask again
+
+}
---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,12 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Diego Figueroa
+ */
+
+public class Solution44 {
+
+    //main
+
+}
